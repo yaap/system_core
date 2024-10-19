14e7e93137506480da4f2c727f3eca78e79cb750
Dyneteve <dyneteve@hentaios.com>
2024-10-19 13:05:11 +0000

    init: Refactor entire SafetyNet spoofing logic
    Spoof later so we can check the build type this allows us to have working
    adb root and remount in userdebug/eng builds also conditionally spoof
    the first API level because API 30 devices and lower use a different
    task_profiles and cgroup configuration that is loaded dynamically.
    
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: Alex Naidis <alex.naidis@linux.com>
    Change-Id: If76d8643b58d291d1fc2383c8af8dbb3f11cdfd8
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>
    [cyberknight777: Drop ro.build.tags and first API level spoofing]
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -118,6 +118,7 @@ static bool persistent_properties_loaded = false;
 static int from_init_socket = -1;
 static int init_socket = -1;
 static bool accept_messages = false;
+static bool weaken_prop_security = false;
 static std::mutex accept_messages_lock;
 static std::mutex selinux_check_access_lock;
 static std::thread property_service_thread;
@@ -407,7 +408,7 @@ static std::optional<uint32_t> PropertySet(const std::string& name, const std::s
         prop_info* pi = (prop_info*)__system_property_find(name.c_str());
         if (pi != nullptr) {
             // ro.* properties are actually "write-once".
-            if (StartsWith(name, "ro.")) {
+            if (StartsWith(name, "ro.") && !weaken_prop_security) {
                 *error = "Read-only property was already set";
                 return {PROP_ERROR_READ_ONLY_PROPERTY};
             }
@@ -1412,12 +1413,29 @@ static void ProcessBootconfig() {
 }
 
 static void SetSafetyNetProps() {
-    // Spoof properties
-    InitPropertySet("ro.boot.flash.locked", "1");
-    InitPropertySet("ro.boot.verifiedbootstate", "green");
-    InitPropertySet("ro.boot.veritymode", "enforcing");
-    InitPropertySet("ro.boot.vbmeta.device_state", "locked");
-    InitPropertySet("ro.build.tags", "release-keys");
+    std::string error;
+    std::string build_type = android::base::GetProperty("ro.build.type", "");
+
+    if (build_type == "user") {
+        // Disable prop security
+        weaken_prop_security = true;
+
+        // Array of property-value pairs to set
+        std::vector<std::pair<std::string, std::string>> properties = {
+            {"ro.boot.flash.locked", "1"},
+            {"ro.boot.verifiedbootstate", "green"},
+            {"ro.boot.veritymode", "enforcing"},
+            {"ro.boot.vbmeta.device_state", "locked"}
+        };
+
+        // Iterate through the vector and set properties
+        for (const auto& prop : properties) {
+            PropertySetNoSocket(prop.first, prop.second, &error);
+        }
+
+        // Restore prop security
+        weaken_prop_security = false;
+    }
 }
 
 void PropertyInit() {
@@ -1434,14 +1452,6 @@ void PropertyInit() {
         LOG(FATAL) << "Failed to load serialized property info file";
     }
 
-    // Report a valid verified boot chain to make Google SafetyNet integrity
-    // checks pass. This needs to be done before parsing the kernel cmdline as
-    // these properties are read-only and will be set to invalid values with
-    // androidboot cmdline arguments.
-    if (!IsRecoveryMode()) {
-      SetSafetyNetProps();
-    }
-
     // If arguments are passed both on the command line and in DT,
     // properties set in DT always have priority over the command-line ones.
     ProcessKernelDt();
@@ -1454,6 +1464,13 @@ void PropertyInit() {
 
     PropertyLoadBootDefaults();
     PropertyLoadDerivedDefaults();
+
+    // Report a valid verified boot chain to make Google SafetyNet integrity
+    // checks pass. This will disable the read only props protection while
+    // being set
+    if (!IsRecoveryMode()) {
+      SetSafetyNetProps();
+    }
 }
 
 static void HandleInitSocket() {
