cdbae9b7cab9dd704b36691dba7ac38155c51f5d
dianlujitao <dianlujitao@lineageos.org>
2025-07-11 16:06:11 +0800

    init: add a hook before parsing system properties
    Also properly implement weak symbol overridden while at it.
    
    Change-Id: Id563c8aaa52c0e41823dbb035f0319a6531f12b7

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1422,6 +1422,8 @@ void PropertyInit() {
     ProcessBootconfig();
     ProcessKernelCmdline();
 
+    vendor_process_bootenv();
+
     // Propagate the kernel variables to internal variables
     // used by init as well as the current required properties.
     ExportKernelBootProps();
--- init/vendor_init.h
+++ init/vendor_init.h
@@ -29,5 +29,6 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef __INIT_VENDOR__H__
 #define __INIT_VENDOR__H__
-extern void vendor_load_properties(void);
+void vendor_load_properties();
+void vendor_process_bootenv();
 #endif /* __INIT_VENDOR__H__ */
