e81b5810b7e2a1f4a07381870e639e1bbcfe450c
John Galt <johngaltfirstrun@gmail.com>
2026-01-12 15:08:33 -0500

    Revert "init: Add vendor-specific initialization hooks."
    For upcoming fixup
    
    This reverts commit ac05557a653be29b4d7c579a17c553e227f08ccc.

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -74,7 +74,6 @@
 #include "subcontext.h"
 #include "system/core/init/property_service.pb.h"
 #include "util.h"
-#include "vendor_init.h"
 
 static constexpr char APPCOMPAT_OVERRIDE_PROP_FOLDERNAME[] =
         "/dev/__properties__/appcompat_override";
@@ -1208,9 +1207,6 @@ void PropertyLoadBootDefaults() {
                        << "' while loading .prop files" << error;
         }
     }
-
-    // Update with vendor-specific property runtime overrides
-    vendor_load_properties();
 
     property_initialize_ro_product_props();
     property_derive_build_fingerprint();
